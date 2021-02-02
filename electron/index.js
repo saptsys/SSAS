6427e9c7e80c8c9cc5038339ca5bc42a39955a16
kdholakiya <kevald47@gmail.com>
2021-02-02 17:00:29 +0530

    Update index.js

--- electron/index.js
+++ electron/index.js
@@ -20,7 +20,7 @@ const createWindow = () => {
   });
 
   // and load the index.html of the app.
-  mainWindow.loadFile(path.join(__dirname, "../../src",'index.html'));
+  mainWindow.loadFile(path.join(__dirname, "../src",'index.html'));
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools();
