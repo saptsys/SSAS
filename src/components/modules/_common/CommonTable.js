0a2a8b639071e021cad085bf798493abbc3221da
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-02-09 23:32:45 +0530

    ++

--- src/components/modules/_common/CommonTable.js
+++ src/components/modules/_common/CommonTable.js
@@ -1,25 +1,20 @@
 import { Table } from 'antd';
-import React, { useEffect, useMemo } from 'react';
+import React from 'react';
 import './CommonTable.less'
 
 const CommonTable = ({ columns, dataSource, loading = false }) => {
-    useEffect(() => {
-        const h = document.getElementById("layout-main-content").clientHeight - (document.getElementsByClassName("ant-table-thead")[0]?.clientHeight ?? 40) - (document.getElementsByClassName("ant-pagination")[0]?.clientHeight ?? 25) - 35;
-        document.querySelector("#table-body tbody").style.maxHeight = h
-    }, [window.innerHeight])
     return (
         <div id="common-table">
             <Table
                 rowKey="id"
-                id="table-body"
                 columns={columns}
                 dataSource={dataSource}
                 size="small"
                 loading={loading}
                 bordered
                 pagination={{ pageSize: 50 }}
-            // scroll={{ y: tableHeight }}
-            // scroll={{ y: "250" }}
+                scroll={{ y: document.getElementById("layout-main-content").clientHeight - (document.getElementsByClassName("ant-table-thead")[0]?.clientHeight ?? 40) - (document.getElementsByClassName("ant-pagination")[0]?.clientHeight ?? 25) - 35 }}
+                // scroll={{ y: "250" }}
             />
         </div>
 
