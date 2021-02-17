1166c45746407abc709e702315e5957bc6605ae6
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-02-17 07:31:47 +0530

    ++

--- dbManager/subscribers/TestSubscriber.js
+++ dbManager/subscribers/TestSubscriber.js
@@ -1,5 +1,6 @@
 const { EventSubscriber } = require('typeorm');
-const { ItemGroupMaster } = require('../models');
+const PartyMasterEntity = require('../entities/PartyMasterEntity');
+const { PartyMaster } = require('../models');
 const DecorateClass = require('../_DecorateClass')
 
 
@@ -10,30 +11,47 @@ class EverythingSubscriber {
      *
      * listenTo?(): Function;
      */
-    // listenTo() {
-    //     return ItemGroupMaster;
-    // }
-
-    /* afterLoad?(entity: Entity): Promise<any>|void; */
-    afterLoad({ entity }) { }
-
-    /* beforeInsert?(event: InsertEvent<Entity>): Promise<any>|void; */
-    beforeInsert({ connection, queryRunner, manager, entity }) {  }
-
-    /* beforeUpdate?(event: UpdateEvent<Entity>): Promise<any>|void; */
-    beforeUpdate({ connection, queryRunner, manager, entity, databaseEntity, updatedColumns, updatedRelations }) { }
 
     /* beforeRemove?(event: RemoveEvent<Entity>): Promise<any>|void; */
-    beforeRemove({ connection, queryRunner, manager, entity, databaseEntity, entityId }) { }
+    beforeRemove(params) {
+        console.log("\n\n--------------BEFORE REMOVE-----------------\n\n")
+    }
+
+    /* afterLoad?(entity: Entity): Promise<any>|void; */
+    afterLoad({ entity }) {
+        console.log("\n\n--------------AFTER Load-----------------\n\n")
+
+    }
+
+    /* beforeInsert?(event: InsertEvent<Entity>): Promise<any>|void; */
+    beforeInsert({ connection, queryRunner, manager, entity }) {
+        console.log("\n\n--------------Before Insert-----------------\n\n")
+
+    }
+
+    /* beforeUpdate?(event: UpdateEvent<Entity>): Promise<any>|void; */
+    beforeUpdate({ connection, queryRunner, manager, entity, databaseEntity, updatedColumns, updatedRelations }) {
+        console.log("\n\n--------------Befaore Update-----------------\n\n")
+
+    }
+
 
     /* afterInsert?(event: InsertEvent<Entity>): Promise<any>|void; */
-    afterInsert({ connection, queryRunner, manager, entity }) { }
+    afterInsert({ connection, queryRunner, manager, entity }) {
+        console.log("\n\n--------------AFTER Insert-----------------\n\n")
+
+    }
 
     /* afterUpdate?(event: UpdateEvent<Entity>): Promise<any>|void; */
-    afterUpdate({ connection, queryRunner, manager, entity }) { }
+    afterUpdate({ connection, queryRunner, manager, entity }) {
+        console.log("\n\n--------------AFTER Update-----------------\n\n")
+
+    }
 
     /* afterRemove?(event: RemoveEvent<Entity>): Promise<any>|void; */
-    afterRemove({ connection, queryRunner, manager, entity }) { }
+    afterRemove(params) {
+        console.log("\n\n--------------AFTER REMOVE-----------------\n\n")
+    }
 }
 
 module.exports = DecorateClass(EventSubscriber(/* Decorator params */), EverythingSubscriber)
