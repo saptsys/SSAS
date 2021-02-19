cd928e3224f7cf9411da9fdb63fe59e156561e3b
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-02-19 22:13:58 +0530

    ++

--- src/components/modules/dashboard/Dashboard.js
+++ src/components/modules/dashboard/Dashboard.js
@@ -1,24 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { LayoutActions } from '../../../_redux/actionFiles/LayoutRedux';
-import { PartyMasterActions, reducerInfo } from '../../../_redux/actionFiles/PartyMasterRedux';
-import PartyMasterForm from '../masters/partyMaster/PartyMasterForm';
-import CommonEditForm from '../_common/CommonEditForm';
 
 const Dashboard = () => {
     const dispatch = useDispatch()
-    useEffect(() => {
+    useEffect(()=>{
         dispatch(LayoutActions.setTitle("Dashboard"))
-    }, [])
+    },[])
     return (
-        <CommonEditForm
-            EditForm={PartyMasterForm}
-            actions={PartyMasterActions}
-            closeDialog={alert}
-            reducerInfo={reducerInfo}
-            titleSufix="Party"
-            editId={2}
-        />
+        <div>
+            This is Dashboard
+        </div>
     );
 };
 
