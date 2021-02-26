63a7fe5e68a9026697564d089c1e7863877507f3
kdholakiya <kevald47@gmail.com>
2021-02-26 23:28:04 +0530

    TASK : settings custom component
    reset button not functioning

--- src/components/table/columnRenderers.js
+++ src/components/table/columnRenderers.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DeleteTwoTone, EditTwoTone } from "@ant-design/icons";
+import { DeleteTwoTone, EditTwoTone, ReloadOutlined } from "@ant-design/icons";
 import { Space, Tag, Tooltip } from "antd";
 
 export const editColumnRenderer = (text, record, index, btnHandler) => (
@@ -21,5 +21,14 @@ export const deleteColumnRenderer = (text, record, index, btnHandler) => (
 export const isActiveColumnRenderer = (text, record, index) => (
     <Space align="center" style={{ cursor: 'pointer' }} >
         {text ? <Tag color="success" >Yes</Tag> : <Tag color="error">No</Tag>}
+    </Space>
+)
+
+
+export const resetColumnRenderer = (text, record, index, btnHandler) => (
+    <Space align="center" style={{ cursor: 'pointer' }}>
+        <Tooltip title="Reset To Default" placement="left">
+            <ReloadOutlined style={{ fontSize: '20px', cursor: 'pointer', '&:hover': { color: "#000" } }} onClick={() => btnHandler && btnHandler(text, record)} />
+        </Tooltip>
     </Space>
 )
