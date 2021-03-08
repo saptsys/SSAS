503249b20fa24af4a9b654e533a781da5a54029b
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-03-08 10:06:32 +0530

    deleted files from src

--- app/src/Helpers/arrayManipulation.js
+++
@@ -1,9 +0,0 @@
-export function searchInArray(array = [], searchText = "") {
-    return array.filter(o =>
-        Object.keys(o).some(k =>
-            String(o[k])
-                .toLowerCase()
-                .includes(searchText.toLowerCase())
-        )
-    );
-}
--- app/src/Helpers/validateMesseges.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    required: '${label} is required!',
-    pattern: '${label} is invalid'
-}
