cd130c3eb30f54885eefbaec9fe7f7f650dafc40
kdholakiya <kevald47@gmail.com>
2021-03-16 10:24:40 +0530

    ++

---
+++ app/electron/services/Reports/GSTR1/Models/B2CSModel.js
@@ -0,0 +1,14 @@
+class B2CSModel {
+  constructor() {
+    this.type = ""
+    this.placeOfSupply = 0
+    this.gstinOfEcom = ""
+    this.rate = 0
+    this.taxableValue = 0
+    this.cessAmount = 0
+  }
+}
+
+module.exports = {
+  B2CSModel:B2CSModel
+}
--- app/electron/services/Reports/GSTR1/Models/index.js
+++ app/electron/services/Reports/GSTR1/Models/index.js
@@ -1,5 +1,7 @@
 const {B2BModel} = require("./B2BModel")
+const {B2CSModel} = require("./B2CSModel")
 
 module.exports = {
-  B2BModel:B2BModel
+  B2BModel:B2BModel,
+  B2CSModel:B2CSModel
 }
