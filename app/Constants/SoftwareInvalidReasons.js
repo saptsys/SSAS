4405e1ffe76d97f354e157f6175d168bba0b7b6e
Vaitul Bhayani <39405950+vaitul@users.noreply.github.com>
2021-04-07 19:05:46 +0530

    ++

--- app/Constants/SoftwareInvalidReasons.js
+++ app/Constants/SoftwareInvalidReasons.js
@@ -1,3 +1,6 @@
+
+// Please do not use special characters because we are passing this type in routing also as params.
+
 export const INVALID_REASONS = {
   DATA_NOT_FOUND: "Data Not Found.",
   SOFTWARE_EXPIRED: "Software is expired.",
@@ -5,5 +8,5 @@ export const INVALID_REASONS = {
   CUSTOMER_INVALID: "Customer is invalid.",
   FIRM_NOT_FOUND: "There are no any valid firm found.",
   DATABASE_NOT_FOUND: "There is no active database found.",
-  MACHINE_ID_NOT_MATCHED: "This software seems copied/moved from other machine/system.",
+  MACHINE_ID_NOT_MATCHED: "This software seems copied or moved from other machine or system.",
 }
